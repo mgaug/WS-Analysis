566287a175d27bbe298be1e87046c1384e0742a4
Markus Gaug <markusgaug@gmail.com>
2024-09-21 14:30:54 +0200

    make script a bit more user-friendly

--- merge_files.py
+++ merge_files.py
@@ -1,9 +1,11 @@
-#import h5py
-#import os
 import pandas as pd
 
-file_names = [ "WS2003-23_long.h5", "WS2023-24_long.h5" ]
-outfile = "WS2003-24_long.h5"
+# This small script shows how to merge different WS files, created by create_files.py
+
+file_names = [ "WS2003-23_long.h5", "WS2023-24_long.h5" ] # two files to be merged 
+outfile = "WS2003-24_long.h5"                             # the new merged output file name
+
+# Do not edit from here on
 
 dff0 = pd.read_hdf(file_names[0])
 dff1 = pd.read_hdf(file_names[1])
@@ -12,20 +14,3 @@ dff = pd.concat([dff0, dff1])
 
 dff.to_hdf(outfile,key="dff",mode="w") #writes data to hdf file
 
-
-
-#d_struct = {} #Here we will store the database structure
-##for i in d_names:
-#  print("Opening: ",i)
-#   f = h5py.File(i,'r+')
-#   print("Keys: ",f.keys())   
-#   d_struct[i] = f.keys()
-#   print ("Structure: ",d_struct[i])
-#   f.close()
-
-#for i in d_names:
-#   print("Treating: ",i)
-#   os.system('h5copy -i %s -o %s -s dff -d dff' % (i, outfile))   
-#   for j  in d_struct[i]:
-#      print("Copying: ",j)      
-#      os.system('h5copy -i %s -o %s -s %s -d %s' % (i, outfile, j, j))
