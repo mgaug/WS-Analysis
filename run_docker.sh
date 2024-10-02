c48d1ba4fffd113c35611f6142dda38dc4a5d9c1
Markus Gaug <markusgaug@gmail.com>
2024-10-02 10:52:40 +0200

    fixed issues

--- run_docker.sh
+++ run_docker.sh
@@ -1,8 +1,2 @@
-docker build -t ws .
-docker images
-docker run -it ws --name ws 
+docker run -v $(pwd)/Results/:/ws/Results/ -it ws
 
-docker ps
-docker exec -it ws bash
-
-docker run -it ws --name ws -v $(pwd)/Results:Results/
